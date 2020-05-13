80cf6af6a7117d33e28a962482feeb59eacd13a5
qus0in <brillante31@naver.com>
2020-05-13 19:09:40 +0900

    snake_case => camelCase

--- programmers/algorithm_practice_kit/hash/완주하지_못한_선수/js_02_refactor.js
+++ programmers/algorithm_practice_kit/hash/완주하지_못한_선수/js_02_refactor.js
@@ -1,19 +1,19 @@
 function solution(participant, completion) {
-    const completion_table = new Map();
+    const completionTable = new Map();
     // for-of를 for-each로 변환
     completion.forEach(v => {
         // if 구문을 삼항 연산자로 축약 
-        completion_table.set(v, completion_table.has(v) ? completion_table.get(v) + 1 : 1);
+        completionTable.set(v, completionTable.has(v) ? completionTable.get(v) + 1 : 1);
     });
     // 정답이 나오면 return 해줘야하기 때문에 for-of를 계속 사용
     // forEach 메소드는 중간에 break가 안 됨, 메소드 사용하고 싶다면 some이나 every 메소드를 참고
     for (const v of participant) {
         // Map의 get 메소드는 키가 없을 경우 Key Error가 아닌 undefined를 return하므로 굳이 별도의 if 연산이 필요하지 않음
         // 그러므로 get 메소드 하나만 하면, key가 없을 때(undefined이므로 false)와 value가 0일 때(false)를 모두 잡아낼 수 있음
-        if (!completion_table.get(v)) {
+        if (!completionTable.get(v)) {
             return v;
         } else {
-            completion_table.set(v, completion_table.get(v) - 1);
+            completionTable.set(v, completionTable.get(v) - 1);
         }
     }
 }
--- programmers/algorithm_practice_kit/hash/완주하지_못한_선수/js_03_clean.js
+++ programmers/algorithm_practice_kit/hash/완주하지_못한_선수/js_03_clean.js
@@ -1,13 +1,13 @@
 function solution(participant, completion) {
-    const completion_table = new Map();
+    const completionTable = new Map();
     completion.forEach(v => {
-        completion_table.set(v, completion_table.has(v) ? completion_table.get(v) + 1 : 1);
+        completionTable.set(v, completionTable.has(v) ? completionTable.get(v) + 1 : 1);
     });
     for (const v of participant) {
-        if (!completion_table.get(v)) {
+        if (!completionTable.get(v)) {
             return v;
         } else {
-            completion_table.set(v, completion_table.get(v) - 1);
+            completionTable.set(v, completionTable.get(v) - 1);
         }
     }
 }
