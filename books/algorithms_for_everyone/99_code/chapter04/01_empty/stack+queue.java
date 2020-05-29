4d9aa568c770bd8274cb0ae22807994a6385b5f1
qus0in <brillante31@naver.com>
2020-05-29 13:20:32 +0900

    (모두의 알고리즘) 큐와 스택 + 프로그래머스 풀이

---
+++ books/algorithms_for_everyone/99_code/chapter04/01_empty/stack+queue.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Main {
+  public static void main(String[] args) {
+    // [큐(Queue)]
+    LinkedList<String> qu = new LinkedList<String>(); // 큐의 생성
+
+    // add() 메소드를 이용한 요소의 저장
+
+    // peek() 메소드를 이용한 요소의 반환
+    
+    // poll() 메소드를 이용한 요소의 반환 및 제거
+
+    // remove() 메소드를 이용한 요소의 제거
+
+    // [스택(Stack)]
+    Stack<Integer> st = new Stack<Integer>(); // 스택의 생성
+
+    // push() 메소드를 이용한 요소의 저장
+
+    // peek() 메소드를 이용한 요소의 반환
+
+    // pop() 메소드를 이용한 요소의 반환 및 제거
+
+  }
+}
